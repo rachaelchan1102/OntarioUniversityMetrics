304fba665e087426b24d7a6f1e350fdae54d61ab
rachaelchan1102 <rachaelchan1102@gmail.com>
2026-02-22 04:06:52 -0500

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import ThemeProvider from '../components/ThemeProvider';
 
 export const metadata: Metadata = {
@@ -10,6 +11,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QZS1W0VTBJ" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QZS1W0VTBJ');
+        `}</Script>
+      </head>
       <body className="bg-gray-50 dark:bg-[#212121] text-gray-900 dark:text-white min-h-screen">
         <ThemeProvider>{children}</ThemeProvider>
       </body>
