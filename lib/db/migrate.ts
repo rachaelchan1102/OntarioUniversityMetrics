5c24e0a34063e0c6b19d61d8c1d53eeb7f22153c
rachaelchan1102 <rachaelchan1102@gmail.com>
2026-03-09 02:59:10 -0400

    Remove all SQLite dependencies - Postgres only

--- lib/db/migrate.ts
+++
@@ -1,7 +0,0 @@
-// DB migration logic
-import { ensureSchema } from './schema';
-
-export function migrate() {
-	ensureSchema();
-	// Add future migrations here
-}
--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Mark better-sqlite3 as external - only used for local dev scripts
-  serverExternalPackages: ['better-sqlite3'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'better-sqlite3'];
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
